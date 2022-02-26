ca63e28531378182383dde4538802c56d2c5604c
Akshay Parakh <akshayparakh98@gmail.com>
2022-02-26 15:59:34 +0530

    Updated to 2.6
    - Added auto chromedriver updating when needed by using webdriver manager.
    - Updated Xpath for message box, it was changed in recent whatsapp web updates.
    - Added dynamic wait to complete uploading of file.

--- whatsapp.py
+++ whatsapp.py
@@ -1,107 +1,131 @@
-'''
------------------------------------------------------
-Author : Akshay Prafull Parakh
-LinkedIn : https://www.linkedin.com/in/akshayparakh98
-Github : https://github.com/AkshayCraZzY
-Instagram : https://www.instagram.com/akki_parakh
-------------------------------------------------------
-'''
+import time
+import sys
+import os
+import os.path
+import mmap
+import warnings
 from selenium import webdriver
+from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-
-import pyperclip
-import time
-import sys
-import os
-import os.path
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import urllib.parse
+os.environ['WDM_LOG_LEVEL'] = '0'
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 options = webdriver.ChromeOptions() 
-options.add_argument("user-data-dir=C:\\Users\\Akshay\\AppData\\Local\\Google\\Chrome\\User Data") 
-options.add_argument('--log-level=3')
-#options.add_argument("--headless") # Runs Chrome in headless mode.
-options.add_argument('--no-sandbox') # Bypass OS security model
-options.add_argument('--disable-gpu')  # applicable to windows os only
-options.add_argument('start-maximized') # 
+options.add_argument("user-data-dir="+os.path.join(sys.path[0])+"/Chrome_Data/User Data") 
+#options.add_argument("window-size=1080x720")
+options.add_argument('log-level=3')
+options.add_argument('disable-gpu')
 options.add_argument('disable-infobars')
-options.add_argument('--disable-extensions')
+options.add_argument('disable-extensions')
+options.add_argument('disable-dev-shm-usage')
+#options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-link="https://web.whatsapp.com/send?phone=91"
-msg="Dear Sir,\nYour account statement has been attached.\nWith Regards,\nFrom AASHIRWAD AGENCIES."
-
+link1="https://web.whatsapp.com/send?phone=91"
+link2="&text=_Dear Sir ,_%0A_Your account statement has been attached._%0A_From_ *Aashirwad Agencies*"
+doc=[]
 
 def beforesend():
     f = open("D:\\Akshay\\AHK\\num.txt", "r")
     num=(f.readline()) 
-    linkk=link+num
+    num=9075886656##########
+    linkk=link1+str(num)+link2
     print(linkk)
     #get_status(w)
     #sys.exit()
-    w = webdriver.Chrome(options=options)
+    w = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
     w.get(linkk)
-    search_xpath = '//div[@contenteditable="true"][@data-tab="3"]'
-    ####//*[@id="app"]/div[1]/span[2]/div[1]/span/div[1]/div/div/div
-    #search_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
-    search_box = WebDriverWait(w, 2000).until(EC.presence_of_element_located((By.XPATH, search_xpath)))
-    print("Srch Found")
-    '''
-    invalid_xpath = '//*[text()="Phone number shared via url is invalid."]'
+    
     try:
-         invalid = WebDriverWait(w, 4000).until(EC.presence_of_element_located((By.XPATH, invalid_xpath)))
-         print("Mobile is Invalid")
-         time.sleep(4)
-         sys.exit()
+        w.find_element_by_class_name("_2UwZ_")#.click()
     except NoSuchElementException:
-        print("Mobile is Valid")
-        #time.sleep(4)
-        #sys.exit()
-    '''
-
-    msg_xpath = '//div[@contenteditable="true"][@data-tab="9"]'
-    msg_box = WebDriverWait(w, 2000).until(EC.presence_of_element_located((By.XPATH, msg_xpath)))
-    print("Msg Found")
-    msg_box.click()
-    pyperclip.copy(msg)
-    msg_box.send_keys(Keys.SHIFT, Keys.INSERT)
-    msg_box.send_keys(Keys.ENTER)
-    print("Message Sent")
-    attach_xpath='//div[@title="Attach"]'
-    attach_btn = WebDriverWait(w, 2000).until(EC.presence_of_element_located((By.XPATH, attach_xpath)))
-    print("Attach Found")
-    attach_btn.click()
-    time.sleep(1)
-    doc_xpath = '//span[@data-icon="attach-document"]'
-    doc_btn = WebDriverWait(w, 2000).until(EC.presence_of_element_located((By.XPATH, doc_xpath)))
-    print("Doc Found")
-    doc_btn.click()
-    filepath = 'D:\Akshay\AHK\sendDoc.ahk'
-    os.startfile(filepath)
-    print("sendDoc.ahk Started")
-    #time.sleep(4)
-    #sys.exit()
-    send_xpath = '//span[@data-icon="send"]'
-    #D:\Akshay\Ledger\PRAFULL_021221.PDF
-    send_btn = WebDriverWait(w, 6000).until(EC.presence_of_element_located((By.XPATH, send_xpath)))
-    print("Send Found")
-    send_btn.click()
-    print("Send Clicked")
-    dwnld_xpath = '//span[@data-testid="audio-download"]'
-    dwnld = WebDriverWait(w, 6000).until(EC.presence_of_element_located((By.XPATH, dwnld_xpath)))
-    print("Download Found")
-    time.sleep(1)
-
-def mainswitch():
-    print("Switch function")
-    if os.path.isfile('D:\\Akshay\\AHK\\docattached'):
-        os.remove("D:\\Akshay\\AHK\\docattached") 
-        beforesend()
+        print(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
     else:
-        beforesend()
+           # cmd = 'color 0c'  
+           # os.system(cmd) 
+        print(" - - -RE-LOGIN NEEDED, Scan QR code and restart script - - -")
+           # cmd = 'color 0a'  
+           # os.system(cmd) 
+        #print(" - - - - - - - - - -WhatsApp Web loaded - - - - - - - - - - ")
+
+    search_xpath = '//div[@contenteditable="true"][@data-tab="3"]'
+        
+    try:
+            
+            WebDriverWait(w, 120).until(EC.presence_of_element_located((By.XPATH, search_xpath)))
+    except TimeoutException:
+            print("WhatsApp could'nt load in 2 minutes")
+            print(tosend)
+    time.sleep(2)
+    try:
+            #element = w.find_element_by_class_name("_2Nr6U").text
+        #print('here')
+        w.find_element_by_class_name("IVxyB")
+        w.find_element_by_class_name("_2Nr6U")
+            #msg_box = WebDriverWait(w, 100).until(EC.presence_of_element_located((By.XPATH, msg_xpath)))
+    except NoSuchElementException:
+        msg_xpath = '//div[@contenteditable="true"][@data-tab="10"]'
+        WebDriverWait(w, 30).until(EC.presence_of_element_located((By.XPATH, msg_xpath))).send_keys(Keys.ENTER)
+           # msg_box.click()
+           # pyperclip.copy(msg)
+           # msg_box.send_keys(Keys.SHIFT, Keys.INSERT)
+           # msg_box.send_keys(Keys.ENTER)
+           # sent = sent + 1
+          #  w.get_screenshot_as_file(os.path.join(sys.path[0])+"\\ss\\"+tosend+"_msg.png")
+            #print('\n - - - - - ['+str(sent)+'] - - - - Message sent to '+tosend+' - - - - - ')
+       # print('\n - - - - - - - - - Message sent to '+tosend+' - - - - - ['+str(sent)+']')
+        #time.sleep(5000)
+        f = open("D:\\Akshay\\AHK\\path.txt", "r")
+        path=(f.readline()) 
+        tosend_doc=path
+        attach_xpath='//div[@title="Attach"]'
+        WebDriverWait(w, 10).until(EC.presence_of_element_located((By.XPATH, attach_xpath))).click()
+        time.sleep(1)
+        doc_xpath = '//span[@data-icon="attach-document"]'
+        send_xpath = '//span[@data-icon="send"]'
+        WebDriverWait(w, 10).until(EC.presence_of_element_located((By.XPATH, doc_xpath)))    
+        w.find_element_by_tag_name('input').send_keys(tosend_doc)
+        WebDriverWait(w, 10).until(EC.presence_of_element_located((By.XPATH, send_xpath))).click()
+        time.sleep(1)
+       # upload_xpath = '//*[@id="main"]/div[3]/div/div[2]/div[3]/div[14]/div/div[2]/div/button/div[2]/div/div[3]/div/div/svg/circle'
+        #upload_xpath = '//*[@id="main"]/div[3]/div/div[2]/div[2]/div[6]/div/div[2]/div/button/div/div/div[3]/div/div/svg/circle'
+        #upload_xpath = '//div[contains(@class, "g0rxnol2 p357zi0d kk3akd72 gndfcl4n ac2vgrno tddarlmj jbxl65f1 mnd5airb")]'
+        #upath= 'aria-label=" Pending "'
+        #u_xpath  = 'g0rxnol2 p357zi0d kk3akd72 gndfcl4n ac2vgrno tddarlmj jbxl65f1 mnd5airb'
+        while True:
+            try:
+                WebDriverWait(w, 1).until(EC.presence_of_element_located((By.XPATH, '//span[@aria-label=" Pending "]')))
+            except TimeoutException:
+                break
+            else:
+                print('Waiting to finish upload..',end="\r")
+                continue
+        print("\n")
+        print('File Sent!')
+
+    try:
+        WebDriverWait(w, 1).until(EC.alert_is_present())
+        alert = w.switch_to.alert
+        alert.accept()
+        print("alert accepted")
+    except TimeoutException:
+        #print("no alert")
+        pass
+
+    #w.execute_script("window.close();")
+    w.quit()
+    return
 
 
-mainswitch()
+
+beforesend()
 
 #sys.exit()
